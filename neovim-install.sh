f75ffee1a7fb1cdb7da2354bc3460a5f11ac137d
junhocho <junhocho@snu.ac.kr>
2016-11-18 22:59:57 +0900

    vimrc, README, neovim-install.sh : remove Vundle and use vim-plug
    Former-commit-id: 891f80936b0fe312dddee45c6d038d8816fcf5be

--- neovim-install.sh
+++ neovim-install.sh
@@ -4,6 +4,9 @@ sudo apt-get install neovim
 # Config changed from neovim
 mkdir ~/.config/nvim/
 echo "source ~/.nvimrc" >> ~/.config/nvim/init.vim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # also make .nvimrc symlink
 ln -s ~/.vimrc ~/.nvimrc
 echo "alias vi='nvim'" >> ~/.zshrc
