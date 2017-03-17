a6e6f526f42c6695005a5a017fc3517aabe3ed94
Junho Cho <junhocho@snu.ac.kr>
2017-03-17 14:47:13 +0900

    change vundle to plug
    Former-commit-id: 1fabc33b92f15cb68e557d5165e23e589cfd20be

--- setup-all.sh
+++ setup-all.sh
@@ -34,16 +34,20 @@ sudo apt-get install vim -y
 # also source vimrc for frequent vimrc update
 echo 'source ~/junhosetting/vimrc' >> ~/.vimrc
 
-# install vundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# install vundle --> changed to plug
+# git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # Some plugins git cloned in the folder
 git clone https://github.com/tbastos/vim-lua.git ~/.vim/bundle/vim-lua
 git clone git://github.com/majutsushi/tagbar ~/.vim/bundle/tagbar
 
-vim +PluginInstall
-
-# Or install Neovim
+# install Neovim
 sh ./neovim-install.sh
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+nvim +PluginInstall
+
 
 # change default shell
 sudo chsh -s /usr/bin/zsh ${USER}
