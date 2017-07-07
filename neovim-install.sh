e8436514aefa46053ed1f69d2b425507429bcef1
junhocho <junhocho@snu.ac.kr>
2017-07-07 16:55:26 +0900

    neovim deprecated link plug

--- neovim-install.sh
+++ neovim-install.sh
@@ -4,10 +4,15 @@ sudo apt-get install neovim
 # Config changed from neovim
 mkdir ~/.config/nvim/
 echo "source ~/.nvimrc" >> ~/.config/nvim/init.vim
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+
+# Old deprcated
+#curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # also make .nvimrc symlink
-ln -s ~/.vimrc ~/.nvimrc
+# ln -s ~/.vimrc ~/.nvimrc
+ln -s ~/.vimrc  ~/.config/nvim/init.vim
 echo "alias vi='nvim'" >> ~/.zshrc
 echo "Now Reload your zsh!"
