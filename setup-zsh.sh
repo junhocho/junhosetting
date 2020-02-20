470e0462782e906f9258e9738b5e0d8d7498a865
junhocho <junhocho@snu.ac.kr>
2020-02-20 18:58:00 +0900

    ohmyzsh

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -1,6 +1,9 @@
 # install zsh and ohmyz.sh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"  # install
 
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+
 # install zsh highlight
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 echo 'source ~/junhosetting/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ${ZDOTDIR:-$HOME}/.zshrc
